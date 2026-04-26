938a0aa2d0f4a0ffe414647db8f7f10e40820841
tanmaywarke1227 <tanmaywarke1227@gmail.com>
2026-04-26 17:23:22 +0530

    new

--- ai-character-detector/src/upload.py
+++ ai-character-detector/src/upload.py
@@ -2,7 +2,7 @@ from huggingface_hub import HfApi
 
 api = HfApi()
 api.upload_folder(
-    folder_path="D:\ai-character-detector\ai-character-detector\src",
+    folder_path=r"D:\ai-character-detector\ai-character-detector\src",
     repo_id="tannmay27/AI-Animated-Character-detection-model",
     repo_type="model"
 )
